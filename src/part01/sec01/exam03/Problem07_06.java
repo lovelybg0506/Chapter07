a3a05a6c86f43d1d2aa5dd269d9492d9c323511a
lovelybg0506 <lovelybg0506@gmail.com>
2021-03-10 23:48:36 +0900

    2021-03-10-11:48

--- src/part01/sec01/exam03/Problem07_06.java
+++ src/part01/sec01/exam03/Problem07_06.java
@@ -28,17 +28,17 @@ public class Problem07_06 {
 				System.out.print("입실하실 방의 번호를 입력 : ");
 				roomNum=in.nextInt();
 				System.out.println(roomNum+"호실에 입실하셨습니다.");
-				room[roomNum]=true;
+				room[roomNum]=check;
 				break;
 			case 2:
 				System.out.print("퇴실하실 방의 번호를 입력 : ");
 				roomNum=in.nextInt();
 				System.out.println(roomNum+"호실에서 퇴실 하셨습니다.");
-				room[roomNum]=false;
+				room[roomNum]=!check;
 				break;
 			case 3:
 				for(int i=0;i<room.length;i++) {
-					if(room[i]=true) {
+					if(room[roomNum]=check) {
 						System.out.println(i+1+"방은 사용중입니다.");
 					}else{
 						System.out.println(i+1+"방은 빈방입니다.");
